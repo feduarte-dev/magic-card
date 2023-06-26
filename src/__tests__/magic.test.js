ee328e088df6a0410e2ced3942fcea433a0eb7fd
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-26 15:48:15 -0300

    segundo requisito pronto

--- src/__tests__/magic.test.js
+++ src/__tests__/magic.test.js
@@ -1,27 +1,30 @@
-// const { getMagicCard } = require('../magic');
-// require('../../startest/mockSetup');
+const { getMagicCard } = require('../magic');
+const { fetchCardById } = require('../magic');
+
+require('../../startest/mockSetup');
 
 jest.setTimeout(10000);
 
 describe('1 - Testes da função getMagicCard', () => {
   it('1.1 Verifique se getMagicCard é uma função.', () => {
-    // implemente seus testes aqui
+    expect(typeof getMagicCard).toBe('function');
   });
 
   it('1.2 Verifique se a função fetch foi chamada.', async () => {
     // await getMagicCard('130550');
-    // implemente seus testes aqui
+   
   });
 
   it('1.3 Verifique se a função fetch foi chamada com o endpoint correto', async () => {
     // await getMagicCard('130550');
-    // implemente seus testes aqui
+    // expect(fetchCardById(130550)).toContain(130550);
   });
 });
 
 describe('2 - Testando o retorno da função getMagicCard', () => {
   it('2.1 Verifique se a carta retornada possui o nome Ancestor\'s Chosen.', async () => {
-    // const response = await getMagicCard('130550');
-    // implemente seus testes aqui
+    const response = await getMagicCard('130550');
+    const { name } = response;
+    expect(name).toContain('Ancestor\'s Chosen');
   });
 });
